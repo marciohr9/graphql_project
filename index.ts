69aedc525e18961151ea595b71993d933e80939f
Márcio Henrique <marciohr9@gmail.com>
2020-10-29 17:51:24 -0300

    configuração base projeto

--- index.ts
+++ index.ts
@@ -1,7 +1,12 @@
-import {createConnection} from "typeorm";
+import {Connection, createConnections} from "typeorm";
 require('dotenv').config({
     path: process.env.NODE_ENV === "dev" ? "config/.conexao.local.env" : "config/.conexao.env"
 });
-console.log(`rodando no ${process.env.DB_MYSQL_NAME}`);
 
-createConnection(require('./config/ormconfig'));
+createConnections([require('./config/ormconfig.mysql'),require('./config/ormconfing.mongo')])
+    .then(()=>{
+        console.log(`conectado em ${process.env.DB_MYSQL_NAME} and ${process.env.DB_MONGO_NAME}`);
+    })
+    .catch((error)=>{
+        if(error) throw error;
+    });
